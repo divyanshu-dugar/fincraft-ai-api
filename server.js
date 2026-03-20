c5219ceaed0c59b5a0120cb6928dab193e4877c8
Divyanshu Dugar <divyu2004@gmail.com>
2026-03-20 10:37:36 -0400

    add async

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 8080;
 
-connectDB();
+(async () => {
+    await connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})();
 
 module.exports = app;
