966da5717f9f9b1940f72d84693783aea22a74dd
Divyanshu Dugar <divyu2004@gmail.com>
2026-04-03 11:44:27 -0400

    add PATCH method

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(cors({
 
     return callback(new Error('CORS: Origin not allowed'));
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
